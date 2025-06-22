517a4044a2a214efea2d448a88bddc049bb7696a
handika27 <handikaptm@gmail.com>
2025-06-22 19:36:23 +0700

    Halaman Aset

--- BackEnd/src/main.js
+++ BackEnd/src/main.js
@@ -1,9 +1,8 @@
-import {web} from "./application/web.js";
-import {logger} from "./application/logging.js";
+import { web } from "./application/web.js";
+import { logger } from "./application/logging.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-
-web.listen(3000, '0.0.0.0', () => {
-    logger.info("Server berjalan di port 3000");
+web.listen(3000, "0.0.0.0", () => {
+  logger.info("Server berjalan di port 3000");
 });

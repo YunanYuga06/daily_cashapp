fc1dafe94c1a38f7272817cd035cc968576f2461
YunanYuga06 <yunanyugapratama@gmail.com>
2026-04-16 22:06:52 +0700

    fix 2

--- BackEnd/src/main.js
+++ BackEnd/src/main.js
@@ -5,6 +5,6 @@ import { logger } from "./application/logging.js";
 // Optional debug: memastikan env kebaca
 logger.info(`SECRET_KEY loaded: ${Boolean(process.env.SECRET_KEY)}`);
 
-web.listen(3000, "0.0.0.0", () => {
+web.listen(8080, "0.0.0.0", () => {
   logger.info("Server berjalan di port 3000");
 });
